a50c1bc8e892f41508e882cee4fcea38832b3889
msara24 <mssaraaa2402@gmail.com>
2026-04-26 16:19:36 +0200

    Add full project

---
+++ Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -0,0 +1,61 @@
+﻿using Common;
+using Modbus.FunctionParameters;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+
+namespace Modbus.ModbusFunctions
+{
+    /// <summary>
+    /// Class containing logic for parsing and packing modbus read holding registers functions/requests.
+    /// </summary>
+    public class ReadHoldingRegistersFunction : ModbusFunction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadHoldingRegistersFunction"/> class.
+        /// </summary>
+        /// <param name="commandParameters">The modbus command parameters.</param>
+        public ReadHoldingRegistersFunction(ModbusCommandParameters commandParameters) : base(commandParameters)
+        {
+            CheckArguments(MethodBase.GetCurrentMethod(), typeof(ModbusReadCommandParameters));
+        }
+
+        /// <inheritdoc />
+        public override byte[] PackRequest()
+        {
+            ModbusReadCommandParameters p = CommandParameters as ModbusReadCommandParameters;
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.Length)), 0, request, 4, 2);
+
+            request[6] = p.UnitId;
+            request[7] = p.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.Quantity)), 0, request, 10, 2);
+
+            return request;
+        }
+
+        /// <inheritdoc />
+        public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
+        {
+            ModbusReadCommandParameters p = CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            for (int i = 0; i < p.Quantity; i++)
+            {
+                ushort value = (ushort)IPAddress.NetworkToHostOrder((short)BitConverter.ToUInt16(response, 9 + i * 2));
+
+                result.Add(
+                    new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, (ushort)(p.StartAddress + i)),
+                    value);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProcessingModule/Acquisitor.cs
@@ -0,0 +1,103 @@
+﻿using Common;
+using System;
+using System.Threading;
+
+namespace ProcessingModule
+{
+    /// <summary>
+    /// Class containing logic for periodic polling.
+    /// </summary>
+    public class Acquisitor : IDisposable
+    {
+        private AutoResetEvent acquisitionTrigger;
+        private IProcessingManager processingManager;
+        private Thread acquisitionWorker;
+        private IStateUpdater stateUpdater;
+        private IConfiguration configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Acquisitor"/> class.
+        /// </summary>
+        /// <param name="acquisitionTrigger">The acquisition trigger.</param>
+        /// <param name="processingManager">The processing manager.</param>
+        /// <param name="stateUpdater">The state updater.</param>
+        /// <param name="configuration">The configuration.</param>
+		public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager, IStateUpdater stateUpdater, IConfiguration configuration)
+        {
+            this.stateUpdater = stateUpdater;
+            this.acquisitionTrigger = acquisitionTrigger;
+            this.processingManager = processingManager;
+            this.configuration = configuration;
+            this.InitializeAcquisitionThread();
+            this.StartAcquisitionThread();
+        }
+
+        #region Private Methods
+
+        /// <summary>
+        /// Initializes the acquisition thread.
+        /// </summary>
+        private void InitializeAcquisitionThread()
+        {
+            this.acquisitionWorker = new Thread(Acquisition_DoWork);
+            this.acquisitionWorker.Name = "Acquisition thread";
+        }
+
+        /// <summary>
+        /// Starts the acquisition thread.
+        /// </summary>
+		private void StartAcquisitionThread()
+        {
+            acquisitionWorker.Start();
+        }
+
+        /// <summary>
+        /// Acquisitor thread logic.
+        /// </summary>
+        private void Acquisition_DoWork()
+        {
+            ushort counter = 0;
+
+            while (true)
+            {
+                acquisitionTrigger.WaitOne();
+
+                foreach (IConfigItem item in configuration.GetConfigurationItems())
+                {
+                    // analogne (3100 i 1500) - svake 3 sekunde
+                    if ((item.StartAddress == 3100 || item.StartAddress == 1500) && counter % 3 == 0)
+                    {
+                        processingManager.ExecuteReadCommand(
+                            item,
+                            counter,
+                            99,
+                            item.StartAddress,
+                            item.NumberOfRegisters
+                        );
+                    }
+                    // digitalne (3400, 3402, 3405, 3406) - svake 4 sekunde
+                    else if ((item.StartAddress == 3400 || item.StartAddress == 3402 || item.StartAddress == 3405 || item.StartAddress == 3406) && counter % 4 == 0)
+                    {
+                        processingManager.ExecuteReadCommand(
+                            item,
+                            counter,
+                            99,
+                            item.StartAddress,
+                            item.NumberOfRegisters
+                        );
+                    }
+                }
+
+                counter++;
+            }
+        }
+
+        #endregion Private Methods
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            acquisitionWorker.Abort();
+        }
+    }
+}
